3f5b4c7f044c660621915dd1caf960d686491645
Minoka Wickramasinghe <induwaraminoka@gmail.com>
2022-08-17 14:51:11 +0530

    Animate background

---
+++ 2d_platformgame/Assets/Scripts/AnimateBc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateBc : MonoBehaviour
+{
+    Material mat;
+    Vector2 movement;
+    public Vector2 speed;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.mainTextureOffset = movement;
+        movement += speed * Time.deltaTime;
+    }
+}
