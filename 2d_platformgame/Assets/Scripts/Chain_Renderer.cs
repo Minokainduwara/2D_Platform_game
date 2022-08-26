923c463fdcf546d2b419fb0c5440af079f21ccc2
Minoka Wickramasinghe <induwaraminoka@gmail.com>
2022-08-27 03:19:10 +0530

    new Animations and Platform Settings

---
+++ 2d_platformgame/Assets/Scripts/Chain_Renderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain_Renderer : MonoBehaviour
+{
+    LineRenderer line;
+    public Transform entryPoint;
+    public Transform existpoint;
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(1, entryPoint.position);
+        line.SetPosition(0, existpoint.position);
+    }
+}
---
+++ 2d_platformgame/Assets/Scripts/Platform_Animation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Animation : MonoBehaviour
+{
+    private Animator _fananim;
+    void Start()
+    {
+        _fananim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            _fananim.Play("Fan_On");
+        }
+    }
+}
